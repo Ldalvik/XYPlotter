7d5c15e46d546ae8877f96d593ccaf6f2f314d59
Julian Osborne <subaruforge@gmail.com>
2018-11-19 09:40:59 -0500

    Update main.java

--- java/main.java
+++ java/main.java
@@ -11,13 +11,17 @@ class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Port port = new Port("COM21");
+        Port port = new Port("COM24");
 
-        String data = parseSvg(readSvg("heart.svg"));
+        String data = parseSvg(readSvg("ok.svg"), "0.875");
         System.out.println(data);
         Plotter p = new Plotter(data);
         System.out.println("-----End of SVG coordinates-----");
 
+        //String xy1 = p.getX(0) + "," + p.getY(0) + "/";
+        //String xy2 = p.getX(1) + "," + p.getY(1) + "/";
+        //System.out.println(xy1);
+        //System.out.println(xy2);
 
         System.out.println("Start drawing? Y/N");
         Scanner scanner = new Scanner(System.in);
@@ -36,13 +40,13 @@ class Main {
                 char charRead = port.read();
                 //System.out.println("rec: " + charRead);
                 if (charRead == '5' && count < p.getLength()) {
-                    String xy = p.getX(count) + "," + p.getY(count) + "/";
-                    System.out.println("(" + count + ")" + xy);
-                    port.send(xy);
-                    port.send("B" + count + ":" + p.getLength() + "/");
+                    String xy = p.getX(count) + "," + p.getY(count);
+                    System.out.println("(" + count + "/" + p.getLength() + ")" + xy);
+                    port.send(xy + "/");
+                    //port.send("B" + count + ":" + p.getLength() + "/");
                     count++;
                 } else {
-                    port.send("BDone!/");
+                    //port.send("BDone!/");
                 }
             }
         } catch (Exception e) {
@@ -53,6 +57,10 @@ class Main {
 
     private static String removeTillWord(String input, String word) {
         return input.substring(input.indexOf(word));
+    }
+
+    public static String removeFromTo(String input, String from, String to){
+        return input.substring(input.indexOf(from), input.indexOf(to));
     }
 
     private static String replace(String input, String word, String replace) {
@@ -71,14 +79,13 @@ class Main {
             }
         }
         return sb.toString();
-
     }
 
-    private static String parseSvg(String data) {
+    private static String parseSvg(String data, String radius) {
         String s1 = removeTillWord(data, "<circle");
         String s2 = replace(s1, "<circle cx=\"", "");
         String s3 = replace(s2, "\" cy=\"", ",");
-        String s4 = replace(s3, "\" r=\"0.5178572\" style=\"fill:none;stroke:black;stroke-width:2;\"/>", "");
+        String s4 = replace(s3, "\" r=\"" + radius + "\" style=\"fill:none;stroke:black;stroke-width:2;\"/>", "");
         String s5 = replace(s4, "</g></g></svg>", "");
         return s5.trim();
     }
