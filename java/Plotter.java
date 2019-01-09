458fee8d5822094b418855d32ea969e406cde0fb
Julian Osborne <subaruforge@gmail.com>
2019-01-09 08:33:18 -0500

    Update Plotter.java

--- java/Plotter.java
+++ java/Plotter.java
@@ -1,54 +1,91 @@
 public class Plotter {
+    private long currentX = 0;
+    private long currentY = 0;
+    private int multiply;
+    public long lowestY;
+    public long highestY;
+    public long lowestX;
+    public long highestX;
+    private SvgParser svg;
 
-    private String[] coordinates;
-    private int currentX = 0;
-    private int currentY = 0;
-
-    public Plotter(String data){
-        this.coordinates = data.split("\n");
+    public Plotter(SvgParser svg, int multiply){
+        this.svg = svg;
+        this.multiply = multiply;
     }
 
-    public int getX(int count){
-        String coordDeci = coordinates[count].replace(".", "");
-        String coord = coordDeci.split(",")[0];
-        String decimal = coord.substring(0, 5);
+    public long getX(int count){
+        String coord = svg.getX(count);
+        String a = coord.substring(coord.indexOf(".") + 1, coord.indexOf(".") + 2);
+        String decimal = coord.substring(0, coord.indexOf(".")).concat(a);
+
         long newX = Long.parseLong(decimal);
         long x;
         if(count==0){
             x = 0;
         } else {
-            x = (newX - currentX);
+            x = (newX - currentX) * multiply;
         }
         /*String xValue = String.valueOf(x);
         if(xValue.length() > 3){
             x = Long.parseLong(String.valueOf(0));
         }*/
-        currentX = (int) newX;
-        return (int) x;
+        currentX = newX;
+        return x;
     }
 
     public long getY(int count){
-        String coordDeci = coordinates[count].replace(".", "");
-        String coord = coordDeci.split(",")[1];
-        String decimal = coord.substring(0, 4);
-
+        String coord = svg.getY(count);
+        String a = coord.substring(coord.indexOf(".") + 1, coord.indexOf(".") + 2);
+        String decimal = coord.substring(0, coord.indexOf(".")).concat(a);
         long newY = Long.parseLong(decimal);
         long y;
         if(count==0){
             y = 0;
         } else {
-            y = (newY - currentY);
+            y = (newY - currentY) * multiply;
+            System.out.print(newY + " - " + currentY + " = " + y + "\t");
         }
 
         /*String yValue = String.valueOf(y);
         if(yValue.length() > 3){
             y = Long.parseLong(String.valueOf(0));
         }*/
-        currentY = (int) newY;
+        currentY = newY;
         return y;
     }
 
-    public int getLength(){
-        return coordinates.length;
+    public void calculate(){
+        String xx1 = svg.getX(0);
+        String xx2 = xx1.substring(0, 4);
+        long xx = Long.parseLong(xx2);
+
+        String yy1 = svg.getX(0);
+        String yy2 = yy1.substring(0, 3);
+        long yy = Long.parseLong(yy2);
+        lowestX = xx;
+        lowestY = yy;
+
+        for(int i = 0; i < svg.getLength(); i++) {
+            String x1 = svg.getX(i);
+            String x2 = x1.substring(0, 4);
+            long x = Long.parseLong(x2);
+
+            String y1 = svg.getX(i);
+            String y2 = y1.substring(0, 3);
+            long y = Long.parseLong(y2);
+
+            if(x > highestX){
+                highestX = x;
+            }
+            if(x < lowestX){
+                lowestX = x;
+            }
+            if(y > highestY){
+                highestY = y;
+            }
+            if(y < lowestY){
+                lowestY = y;
+            }
+        }
     }
 }
