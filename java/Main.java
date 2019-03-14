5b51b2b47ce3fdb874d83d33ed30c9e90d6afff4
Julian Osborne <subaruforge@gmail.com>
2019-03-14 10:09:45 -0400

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            new PlotterServer(8080, "/dev/ttyUSB0", 1000);
+            new PlotterServer(8080, "/dev/ttyUSB0", 1000, "webserver.html");
         } catch (IOException e) {
             System.err.println("Couldn't start server:\n" + e);
         }
