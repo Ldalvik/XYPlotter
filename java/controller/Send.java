8a59563faeec966af26b43c67a44775f17d6a1bb
Julian Osborne <subaruforge@gmail.com>
2019-03-18 09:15:56 -0400

    Add files via upload

--- java/controller/Send.java
+++ java/controller/Send.java
@@ -11,31 +11,39 @@ public class Send {
         this.port = port;
     }
 
+    //Sets int X coordinate
     public Send setX(int x){
         this.x = x;
         return this;
     }
 
+    //Sets long X coordinate
     public Send setX(long x){
         this.x = Math.toIntExact(x);
         return this;
     }
 
+    //Sets int Y coordinate
     public Send setY(int y){
         this.y = y;
         return this;
     }
 
+    //Sets long Y coordinate
     public Send setY(long y){
         this.y = Math.toIntExact(y);
         return this;
     }
 
-    public Send tap(){
+    //Sends tap request
+    public void tap(){
         port.send("tap/");
-        return this;
     }
 
+    /*
+      Sends request to move the given X and Y value (if isManual == false, then the arduino will
+      confirm when its done plotting. If true, it will not confirm that its done, which is used for manual control.
+    */
     public void sendMessage(boolean isManual){
         if(isManual){
             port.send("MANUAL:" + x + "," + y + "/");
